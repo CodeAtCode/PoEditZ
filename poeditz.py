253a7d5a34da266a19501381f59fda1dcf865586
Daniele Scasciafratte <mte90net@gmail.com>
2017-06-20 12:13:10 +0200

    merge works

--- poeditz.py
+++ poeditz.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 import polib, sys, os, random
 
-po = polib.pofile(sys.argv[1]) 
-temp_name = '/tmp/temp_' + str(random.randint(0,999)) + '.po'
+po_file = sys.argv[1]
+po = polib.pofile(po_file)
+temp_name = '/tmp/temp_' + str(random.randint(0, 999)) + '.po'
 os.system('cp ' + sys.argv[1] + ' ' + temp_name)
 headers = po.ordered_metadata()
 folder = '/var/www/VVV/www/demo/htdocs/wp-content/plugins/glossary-pro/'
 
+
 def is_excluded(folder, file, excluded):
     for exclude in excluded:
         if exclude.endswith('.php') and file.endswith('.php'):
-            if exclude in os.path.join(folder,file):
+            if exclude in os.path.join(folder, file):
                 return True
         elif exclude in folder:
             return True
@@ -20,18 +22,24 @@ files_list = ''
 excluded = []
 keyword = ''
 
-for (header,value) in headers:
+for (header, value) in headers:
     if header.startswith('X-Poedit'):
         if header.startswith('X-Poedit-KeywordsList'):
-            keyword = '--keyword=' + value.replace(';',',') + ''
+            keyword = '--keyword=' + value.replace(';', ',') + ''
         if header.startswith('X-Poedit-SearchPathExcluded'):
             excluded.append(value)
-            
+
 for root, dirs, files in os.walk(folder):
     for file in files:
         if not is_excluded(root, file, excluded):
             if file.endswith('.php'):
                 files_list += os.path.join(root, file) + ' '
 
-command = 'xgettext ' + keyword + ' --force-po --debug -c --join-existing --output=' + temp_name + ' ' + files_list
+command = 'xgettext ' + keyword + ' --force-po --debug -c --join-existing'
+command += ' --output=' + temp_name + ' ' + files_list
+command += ' | msgmerge -N ' + po_file + ' ' + temp_name
+command += ' --output=' + temp_name + ' > /dev/null'
+os.system(command)
+
+command = 'mv ' + temp_name + ' ' + po_file
 os.system(command)
